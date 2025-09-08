fc6a179620fa5264bd30f726afc9b34105916d1b
Junior <lightgraphics80@gmail.com>
2025-09-08 22:35:20 +0000

    feat: adiciona lista de animais e método converteStringEmLista para processar strings de entrada

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,7 +1,19 @@
 class AbrigoAnimais {
+  animais = {
+    Rex:  { tipo: "cão",    brinquedos: ["RATO", "BOLA"] },
+    Mimi: { tipo: "gato",   brinquedos: ["BOLA", "LASER"] },
+    Fofo: { tipo: "gato",   brinquedos: ["BOLA", "RATO", "LASER"] },
+    Zero: { tipo: "gato",   brinquedos: ["RATO", "BOLA"] },
+    Bola: { tipo: "cão",    brinquedos: ["CAIXA", "NOVELO"] },
+    Bebe: { tipo: "cão",    brinquedos: ["LASER", "RATO", "BOLA"] },
+    Loco: { tipo: "jabuti", brinquedos: ["SKATE", "RATO"] }  
+  }
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
   }
-}
 
+  converteStringEmLista(texto) {
+    if (!texto) return [];
+    return texto.split(",").map(texto => texto.trim());
+  }
 export { AbrigoAnimais as AbrigoAnimais };
